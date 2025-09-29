c1a741bd9db54bd871170565e19c06b859cb8c24
Audrey <167112263+ralphwolckoff@users.noreply.github.com>
2025-09-29 06:28:39 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: "https://e-commerce-95zu1yfif-ralphs-projects-741b8b37.vercel.app/",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
